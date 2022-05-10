837796b497c3fb3563f8b280329c11b316c8a9e6
Beibei(Bella) Du <60382493+belladu0201@users.noreply.github.com>
2022-05-09 21:06:46 -0700

    yea first medium that takes not much time

--- leetcode_practice.py
+++ leetcode_practice.py
@@ -19,7 +19,33 @@ def maxProfit(self, prices: List[int]) -> int:
         min_val = min(min_val,i)
         diff.append(cur-min_val)
     return max(diff)
+
 # 217. Contains Duplicate
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         return len(nums) != len(set(nums))
+    
+# 238. Product of Array Except Self
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        temp,product = [],1
+        zero = nums.count(0)
+        for i in nums:
+            if zero == 0:
+                product *= i
+            else:
+                if i != 0:
+                    product *= i
+        for i,e in enumerate(nums):
+            if zero == 0:
+                temp.append(int(product/e))
+            else:
+                if (zero > 1):
+                    temp.append(0)
+                elif (zero == 1):
+                    if e == 0:
+                        temp.append(int(product))
+                    else:
+                        temp.append(0)
+        return temp
+
