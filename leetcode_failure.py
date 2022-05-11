fd5691a1a74c3a5df96a52c288cb686564cff6ac
Beibei(Bella) Du <60382493+belladu0201@users.noreply.github.com>
2022-05-11 14:31:35 -0700

    failed code, haven't figured it out yet

--- leetcode_failure.py
+++ leetcode_failure.py
@@ -67,3 +67,51 @@ class Solution:
                 return len(s)
             return max(start_idx,find_re(s[1:]))
         return find_re(s)
+
+    # 152. Maximum Product Subarray
+    class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        counter = 0
+        lst = []
+        prod = 1
+        if len(nums) == 1:
+            return nums[0]
+        for i in nums:
+            if i<0:
+                counter += 1
+                lst.append(i)
+        if counter == 0:
+            for i in nums:
+                if i != 0:
+                    prod *= i
+        elif counter % 2 == 0:
+            for i in nums:
+                prod*=i
+        else:
+            #prod1,prod2,prod3,prod4 = 1,1,1,1
+            temp1 =nums[:nums.index(lst[0])+1]
+            temp2 =nums[nums.index(lst[0])+1:]
+            temp3 = nums[:nums.index(lst[-1])+1]
+            temp4 = nums[nums.index(lst[-1])+1:]
+            print([temp1, temp2,temp3,temp4])
+            for i in temp1:
+                prod *= i
+            #print(prod)
+            lst.append(prod)
+            prod = 1
+            for j in temp2:
+                prod *= j
+            lst.append(prod)
+            prod = 1
+            for k in temp3:
+                prod *= k
+            lst.append(prod)
+            prod = 1
+            for l in temp4:
+                prod *= l
+            lst.append(prod)
+            print(lst)
+            prod = max(lst)
+        return prod
+            
+            
