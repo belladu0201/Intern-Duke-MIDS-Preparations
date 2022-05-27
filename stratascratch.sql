314cfd6c561ed6f14cc7fab33b94c20f5b54ed23
Beibei(Bella) Du <60382493+belladu0201@users.noreply.github.com>
2022-05-27 02:52:16 -0700

    Update stratascratch.sql

--- stratascratch.sql
+++ stratascratch.sql
@@ -3,3 +3,11 @@ select activity_date,  pe_description
 from los_angeles_restaurant_health_inspections
 WHERE facility_name = 'STREET CHURROS'
     and score < 95
+   
+   
+--Highest Salary
+SELECT 
+    first_name, salary
+from 
+    worker
+WHERE salary = (SELECT max(salary) from worker)
