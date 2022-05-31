7f19bcafeb765652df150bda927dffa5609ebed9
Beibei(Bella) Du <60382493+belladu0201@users.noreply.github.com>
2022-05-31 03:27:02 -0700

    reverse sll
    i just copy answer

--- leetcode_practice.py
+++ leetcode_practice.py
@@ -259,3 +259,19 @@ class Solution(object):
                 return []
             return inorder_traversal(node.left) + [node.val] + inorder_traversal(node.right)
         return inorder_traversal(root)        
+# 206 Reverse Linked List
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        curr = head
+        prev = None
+        while curr != None:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        return prev
