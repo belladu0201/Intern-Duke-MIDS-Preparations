62d918844afca60820847900fa610a2576e40c06
Beibei(Bella) Du <60382493+belladu0201@users.noreply.github.com>
2022-08-08 22:02:26 -0700

    stupid question

--- leetcode_practice.py
+++ leetcode_practice.py
@@ -373,4 +373,22 @@ class Solution:
             l1 += nums[i]
             
         return -1
-            
+        
+   # 412. Fizz Buzz
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        #temp =  [str(i) for i in range(1,n+1)]
+        output = []
+        for i in range(1,n+1):
+            if (i % 3 == 0) and (i % 5 == 0):
+                output.append("FizzBuzz")
+            elif i % 3 == 0:
+                output.append("Fizz")
+            elif i % 5 == 0:
+                output.append("Buzz")
+            else:
+                output.append(str(i))
+        return output
+                
+    
+        
