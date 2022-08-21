cacb1f50873b0cf20fd07b487a51483e6b1b3f21
Beibei(Bella) Du <60382493+belladu0201@users.noreply.github.com>
2022-08-21 02:32:36 -0400

    dont undersand the else

--- leetcode_practice.py
+++ leetcode_practice.py
@@ -634,3 +634,20 @@ class Solution(object):
             p1 = headB if p1 is None else p1.next
             p2 = headA if p2 is None else p2.next           
         return p1
+# 728. Self Dividing Numbers
+class Solution(object):
+    def selfDividingNumbers(self, left, right):
+        """
+        :type left: int
+        :type right: int
+        :rtype: List[int]
+        """
+        # using brute force
+        temp = []
+        for i in range(left,right + 1):
+            for j in str(i):
+                if int(j) == 0 or i % int(j)!=0:
+                    break
+            else:
+                temp.append(i)
+        return temp
