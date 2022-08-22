e17900fe0d3ce06b0816805665e8c0244b398e7b
Beibei(Bella) Du <60382493+belladu0201@users.noreply.github.com>
2022-08-21 22:35:35 -0400

    another easy sql

--- leetcode.sql
+++ leetcode.sql
@@ -1,4 +1,10 @@
-1757. Recyclable and Low Fat Products
+-- 1757. Recyclable and Low Fat Products
 # Write your MySQL query statement below
 Select product_id from Products
 where (low_fats = 'Y') and (recyclable = 'Y')
+
+-- 1741. Find Total Time Spent by Each Employee
+# Write your MySQL query statement below
+SELECT event_day as day, emp_id, sum(out_time - in_time) as total_time
+FROM Employees
+GROUP BY emp_id, day
