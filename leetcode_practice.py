f5aa7bc081fcedf6b0166ebd21c8c88d854067a4
Beibei(Bella) Du <60382493+belladu0201@users.noreply.github.com>
2022-08-22 10:53:16 -0400

    new prolem easy

--- leetcode_practice.py
+++ leetcode_practice.py
@@ -676,3 +676,16 @@ class Solution(object):
             else:
                 temp.append(False)
         return temp
+# 1313. Decompress Run-Length Encoded List
+class Solution(object):
+    def decompressRLElist(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        temp = []
+        for i in range(0,len(nums),2):
+            for j in range(nums[i]):
+                temp.append(nums[i+1])
+            
+        return temp
