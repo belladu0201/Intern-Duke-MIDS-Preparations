42e8c45d69592e9fe71afd7ddab155b79dff8f63
Beibei(Bella) Du <60382493+belladu0201@users.noreply.github.com>
2022-08-30 00:01:04 -0400

    checked sol

--- leetcode_practice.py
+++ leetcode_practice.py
@@ -1112,3 +1112,13 @@ class Solution:
             l +=1
             r -=1
         return True
+# 1710. Maximum Units on a Truck
+class Solution:
+    def maximumUnits(self, boxTypes: List[List[int]], truckSize: int) -> int:
+        boxTypes.sort(key=lambda x: -x[1])
+        cur_size = 0
+        max_units = 0
+        for num_box, unit in boxTypes:
+            max_units += unit * min(truckSize - cur_size, num_box)
+            cur_size += min(truckSize - cur_size, num_box)
+        return max_units
