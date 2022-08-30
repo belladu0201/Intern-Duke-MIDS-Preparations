cf51f8956c404fc50371274c65c4cc4a7738960c
Beibei(Bella) Du <60382493+belladu0201@users.noreply.github.com>
2022-08-30 16:41:03 -0400

    better easy sol

--- leetcode_practice.py
+++ leetcode_practice.py
@@ -1160,3 +1160,10 @@ class Solution:
             if len(list(i)) == len(list(set(i))):
                 ct += 1
         return ct
+    #########class Solution:
+    def countGoodSubstrings(self, s: str) -> int:
+        count = 0
+        for i in range(len(s)-2):
+            if len(set([s[i],s[i+1],s[i+2]])) == 3:
+                count+=1
+        return count
