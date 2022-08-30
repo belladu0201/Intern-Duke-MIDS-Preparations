b5f3c840b9665592e52b02ec3588519673622351
Beibei(Bella) Du <60382493+belladu0201@users.noreply.github.com>
2022-08-30 00:24:31 -0400

    1 is customer id

--- leetcode.sql
+++ leetcode.sql
@@ -225,3 +225,11 @@ FROM
 Visits
 where visit_id not in (select visit_id from transactions)
 group by 1
+--------------
+# Write your MySQL query statement below
+select
+customer_id, count(distinct visit_id) as count_no_trans
+FROM
+Visits
+where visit_id not in (select visit_id from transactions)
+group by customer_id
