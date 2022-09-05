c9b83ab828fd476b85177a2c5db3e08cea4299b5
Beibei(Bella) Du <60382493+belladu0201@users.noreply.github.com>
2022-09-05 16:56:11 -0400

    refer to sol. why n == 0 needed

--- leetcode_practice.py
+++ leetcode_practice.py
@@ -1423,3 +1423,15 @@ class Solution:
             if intervals[i][1] > intervals[i+1][0]:
                 return False
         return True
+# 263. Ugly Number
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        if n == 0: return False
+        while n % 2 == 0:
+            n /= 2
+        while n % 3 == 0:
+            n /= 3
+        while n % 5 == 0:
+            n /= 5
+            
+        return n == 1
