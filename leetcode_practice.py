fd7629e9312b15b98ad1f09547393115e30e8296
Beibei(Bella) Du <60382493+belladu0201@users.noreply.github.com>
2022-09-05 18:07:20 -0400

    preorder traversal refer to sol. completely

--- leetcode_practice.py
+++ leetcode_practice.py
@@ -1476,3 +1476,22 @@ class Solution:
             #print(temp[i])
                 output.append(temp)
         return output
+# 108. Convert Sorted Array to Binary Search Tree
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+        def helper(left,right):
+            if left > right:
+                return None
+        # Preorder
+            m = (left + right) // 2
+            root = TreeNode(nums[m])
+            root.left = helper(left,m-1)
+            root.right = helper(m+1,right)
+            return root
+        return helper(0,len(nums)-1)
