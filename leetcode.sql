08fd03847a0a61adaf51b30afd8bdc9667eb3d17
Beibei(Bella) Du <60382493+belladu0201@users.noreply.github.com>
2022-09-06 02:03:15 -0400

    refer to sol.

--- leetcode.sql
+++ leetcode.sql
@@ -301,3 +301,20 @@ left join NPV n using(id,year)
 # Write your MySQL query statement below
 SELECT *, if((x+y) > z and (x+z) > y and (y + z) > x, 'Yes', 'No') as triangle
 FROM Triangle
+
+-- 619. Biggest Single Number
+# Write your MySQL query statement below
+select max(num) as num from (
+    select num from MyNumbers
+    group by num
+    having count(num) = 1
+) as t
+# SELECT
+#     MAX(num) AS num
+# FROM
+#     (SELECT
+#         num
+#     FROM
+#         MyNumbers
+#     GROUP BY num
+#     HAVING COUNT(num) = 1) AS t;
