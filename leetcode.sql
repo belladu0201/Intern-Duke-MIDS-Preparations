8f523cbf996d7c1769fde421f4691d0ccd2887fe
Beibei(Bella) Du <60382493+belladu0201@users.noreply.github.com>
2022-09-05 23:12:05 -0400

    Update leetcode.sql

--- leetcode.sql
+++ leetcode.sql
@@ -296,3 +296,8 @@ where amount > 500
 select q.id, q.year, ifnull(npv,0) as npv
 from Queries q
 left join NPV n using(id,year)
+
+-- 610. Triangle Judgement
+# Write your MySQL query statement below
+SELECT *, if((x+y) > z and (x+z) > y and (y + z) > x, 'Yes', 'No') as triangle
+FROM Triangle
