509fbc019efa736d7e529593f9b67fc8764ef5b3
Beibei(Bella) Du <60382493+belladu0201@users.noreply.github.com>
2022-10-01 21:07:03 -0400

    notice the index

--- leetcode_practice.py
+++ leetcode_practice.py
@@ -1906,3 +1906,12 @@ class Solution:
                 ct += 1
         return ct
             
+# 766. Toeplitz Matrix
+class Solution:
+    def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
+        # find the index
+        for i in range(len(matrix)-1):
+            for j in range(len(matrix[i])-1):
+                if matrix[i][j] != matrix[i+1][j+1]:
+                    return False
+        return True
