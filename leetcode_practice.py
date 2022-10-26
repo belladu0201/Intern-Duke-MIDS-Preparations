e86e055b43ee9515a698368e94bc5e8195de60fb
Beibei(Bella) Du <60382493+belladu0201@users.noreply.github.com>
2022-10-25 18:18:39 -0700

    dp; refer to sol

--- leetcode_practice.py
+++ leetcode_practice.py
@@ -2182,3 +2182,16 @@ class Solution:
                     break
         print(dp)       
         return dp[len(s)] # why to check the last index
+
+
+# 746. Min Cost Climbing Stairs
+class Solution(object):
+    def minCostClimbingStairs(self, cost):
+        """
+        :type cost: List[int]
+        :rtype: int
+        """
+        dp = [0] * (len(cost) + 1)
+        for i in range(2,len(cost)+1):
+            dp[i] = min(cost[i-1] + dp[i-1],cost[i-2] + dp[i-2])
+        return dp[-1]
