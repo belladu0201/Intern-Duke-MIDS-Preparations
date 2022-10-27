0c72fd49f2fbc7f7b071a12d0334fbc491e720d6
Beibei(Bella) Du <60382493+belladu0201@users.noreply.github.com>
2022-10-26 21:07:10 -0700

    upload last dp question

--- leetcode_practice.py
+++ leetcode_practice.py
@@ -2213,3 +2213,22 @@ class Solution(object):
             #print([i])
             dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
         return dp[n]
+
+# 1646. Get Maximum in Generated Array
+class Solution:
+    def getMaximumGenerated(self, n: int) -> int:
+        dp = [0] * (n+1)
+        if n >= 2:
+            dp[0] = 0
+            dp[1] = 1
+            if n % 2 == 0:
+                for i in range(1,n//2):
+                    dp[2 * i] = dp[i]
+                    dp[2 * i + 1] = dp[i] + dp[i + 1]
+            else:
+                 for i in range(1,n//2+1):
+                    dp[2 * i] = dp[i]
+                    dp[2 * i + 1] = dp[i] + dp[i + 1]
+        else: return [0,1][n]
+        return max(dp)
+    # the case of 2 does not work, ok works now
