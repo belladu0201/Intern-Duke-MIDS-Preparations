cd499d575af0e83f2735eb72e1b84c287957c226
Beibei(Bella) Du <60382493+belladu0201@users.noreply.github.com>
2022-11-10 00:34:07 -0800

    wrong intepretation at firstl; not happy still

--- leetcode_practice.py
+++ leetcode_practice.py
@@ -2300,3 +2300,20 @@ class Solution(object):
             if ct == len(i):
                 nums += 1
         return nums
+# 1534. Count Good Triplets
+class Solution(object):
+    def countGoodTriplets(self, arr, a, b, c):
+        """
+        :type arr: List[int]
+        :type a: int
+        :type b: int
+        :type c: int
+        :rtype: int
+        """
+        ct = 0
+        for i in range(len(arr)):
+            for j in range(i+1,len(arr)):
+                for k in range(j+1,len(arr)):
+                    if (abs(arr[i] - arr[j]) <= a) & (abs(arr[j] - arr[k]) <= b) & (abs(arr[i] - arr[k]) <= c):
+                        ct += 1
+        return ct
