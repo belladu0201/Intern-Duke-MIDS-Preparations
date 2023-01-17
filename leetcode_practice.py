7029b58933fdc018203010e11103f8736e24d198
Beibei(Bella) Du <60382493+belladu0201@users.noreply.github.com>
2023-01-16 22:55:13 -0800

    refer to sol. binary tree

--- leetcode_practice.py
+++ leetcode_practice.py
@@ -2903,3 +2903,25 @@ class Solution(object):
         if (root.val < val):
             return self.searchBST(root.right,val)
         return root
+# 938. Range Sum of BST
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def rangeSumBST(self, root, low, high):
+        """
+        :type root: TreeNode
+        :type low: int
+        :type high: int
+        :rtype: int
+        """
+        if root == None:
+            return 0
+        elif root.val < low:
+            return self.rangeSumBST(root.right, low,high)
+        elif root.val > high:
+            return self.rangeSumBST(root.left, low,high)
+        return root.val + self.rangeSumBST(root.right, low,high) + self.rangeSumBST(root.left, low,high)
