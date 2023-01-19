1d7c6341dfcbe85fd11973dda870eef5d4289d0c
Beibei(Bella) Du <60382493+belladu0201@users.noreply.github.com>
2023-01-19 07:51:53 -0800

    look at solution

--- leetcode_practice.py
+++ leetcode_practice.py
@@ -2925,3 +2925,26 @@ class Solution(object):
         elif root.val > high:
             return self.rangeSumBST(root.left, low,high)
         return root.val + self.rangeSumBST(root.right, low,high) + self.rangeSumBST(root.left, low,high)
+
+# 226. Invert Binary Tree
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        if not root:
+            return None
+        r = self.invertTree(root.right)
+        l = self.invertTree(root.left)
+        root.left = r
+        root.right = l
+        return root
